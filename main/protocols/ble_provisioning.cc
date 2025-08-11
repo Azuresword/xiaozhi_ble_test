f8765b69d91a8a2c4a4f72a9af15e6d488c03556
dylan <1577856435@qq.com>
2025-08-11 16:13:51 +0800

    fix: char to static

--- main/protocols/ble_provisioning.cc
+++ main/protocols/ble_provisioning.cc
@@ -6,6 +6,7 @@
 #include "services/gatt/ble_svc_gatt.h"
 #include "host/util/util.h"
 
+#include "cJSON.h"
 static const char *TAG = "BleProvisioning";
 
 // UUIDs from your specification
@@ -16,7 +17,10 @@ static const ble_uuid128_t gatt_svr_chr_uuid =
     BLE_UUID128_INIT(0xFB, 0x34, 0x9B, 0x5F, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xB1, 0xBB, 0x00, 0x00);
 
 // C-style pointer to our instance to be used in static callbacks
+
 static BleProvisioning* instance_ptr = nullptr;
+
+static uint16_t gatt_svr_chr_val_handle;
 
 // --- CORRECTED SECTION ---
 // First, define the characteristics array as a separate static const variable.
@@ -26,7 +30,7 @@ static const struct ble_gatt_chr_def gatt_svr_characteristics[] = {
         .access_cb = BleProvisioning::gatt_svr_access_cb,
         .flags = BLE_GATT_CHR_F_WRITE | BLE_GATT_CHR_F_NOTIFY | BLE_GATT_CHR_F_READ,
         // This is now safe because instance_ptr is set before this struct is used by the stack.
-        .val_handle = &(instance_ptr->characteristic_val_handle_)
+        .val_handle = &gatt_svr_chr_val_handle
     },
     {0} /* End of characteristics */
 };
@@ -75,7 +79,7 @@ void BleProvisioning::start() {
     }
     
     // Set the device name
-    rc = ble_svc_gap_device_name_set("XiaoZhi-Provisioning");
+    rc = ble_svc_gap_device_name_set("XZP");
     if (rc != 0) {
         ESP_LOGE(TAG, "Failed to set device name: %d", rc);
     }
@@ -115,27 +119,42 @@ void BleProvisioning::on_sync() {
 
 void BleProvisioning::advertise() {
     struct ble_gap_adv_params adv_params;
-    memset(&adv_params, 0, sizeof(adv_params));
-    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
-    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    struct ble_hs_adv_fields fields;
+    const char *name;
+    int rc;
 
-    const uint8_t adv_data_val[] = {
-        0x02, 0x01, 0x06, // Flags: LE General Discoverable Mode, BR/EDR Not Supported
-        0x03, 0x03, 0xAA, 0xAA // 16-bit service UUID (shortened from 128-bit)
-    };
-    
-    int rc = ble_gap_adv_set_data(adv_data_val, sizeof(adv_data_val));
+    memset(&fields, 0, sizeof fields);
+
+    /* Advertise two things:
+     * - General discoverability.
+     * - Our name.
+     */
+    fields.flags = BLE_HS_ADV_F_DISC_GEN |
+                   BLE_HS_ADV_F_BREDR_UNSUP;
+
+    /* Include our complete name in the advertising packet */
+    name = ble_svc_gap_device_name();
+    fields.name = (uint8_t *)name;
+    fields.name_len = strlen(name);
+    fields.name_is_complete = 1;
+
+    rc = ble_gap_adv_set_fields(&fields);
     if (rc != 0) {
-        ESP_LOGE(TAG, "ble_gap_adv_set_data failed: %d", rc);
+        ESP_LOGE(TAG, "error setting advertising data; rc=%d", rc);
         return;
     }
 
-    rc = ble_gap_adv_start(own_addr_type_, NULL, BLE_HS_FOREVER, &adv_params, gap_event_cb, this);
+    /* Begin advertising. */
+    memset(&adv_params, 0, sizeof adv_params);
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    rc = ble_gap_adv_start(own_addr_type_, NULL, BLE_HS_FOREVER, &adv_params,
+                           gap_event_cb, this);
     if (rc != 0) {
-        ESP_LOGE(TAG, "ble_gap_adv_start failed: %d", rc);
-    } else {
-        ESP_LOGI(TAG, "Advertising started...");
+        ESP_LOGE(TAG, "error enabling advertising; rc=%d", rc);
+        return;
     }
+     ESP_LOGI(TAG, "Advertising started with name: %s", name);
 }
 
 int BleProvisioning::gap_event_cb(struct ble_gap_event *event, void *arg) {
@@ -171,28 +190,42 @@ void BleProvisioning::handle_gap_event(struct ble_gap_event *event) {
 }
 
 int BleProvisioning::gatt_svr_access_cb(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg) {
-    if (instance_ptr && attr_handle == instance_ptr->characteristic_val_handle_) {
+    // *** 修改点 3: 使用静态句柄进行比较 ***
+    if (attr_handle == gatt_svr_chr_val_handle) {
         if (ctxt->op == BLE_GATT_ACCESS_OP_WRITE_CHR) {
             ESP_LOGI(TAG, "Received write request on characteristic");
-            instance_ptr->handle_gatt_write(conn_handle, ctxt->om);
+            if (instance_ptr) {
+                instance_ptr->handle_gatt_write(conn_handle, ctxt->om);
+            }
         }
     }
     return 0;
 }
 
 void BleProvisioning::handle_gatt_write(uint16_t conn_handle, struct os_mbuf *om) {
-    // Placeholder for handling incoming JSON data
     uint16_t len = OS_MBUF_PKTLEN(om);
     char* data = (char*)malloc(len + 1);
-    if (data) {
-        int rc = ble_hs_mbuf_to_flat(om, data, len, NULL);
-        if (rc == 0) {
-            data[len] = '\0';
-            ESP_LOGI(TAG, "Data received: %s", data);
-            // Here we will parse the JSON and dispatch commands
+    if (!data) return;
+
+    if (ble_hs_mbuf_to_flat(om, data, len, NULL) == 0) {
+        data[len] = '\0';
+        ESP_LOGI(TAG, "Data received: %s", data);
+
+        // 使用cJSON库解析收到的字符串
+        cJSON *root = cJSON_Parse(data);
+        if (root) {
+            cJSON *ssid_item = cJSON_GetObjectItem(root, "ssid");
+            cJSON *password_item = cJSON_GetObjectItem(root, "password");
+
+            // 如果成功解析出ssid和password，并且回调函数已经设置
+            if (ssid_item && password_item && on_provisioned_cb_) {
+                // 就调用回调函数，把凭据传递出去
+                on_provisioned_cb_(ssid_item->valuestring, password_item->valuestring);
+            }
+            cJSON_Delete(root);
         }
-        free(data);
     }
+    free(data);
 }
 
 void BleProvisioning::on_provisioned(std::function<void(const std::string& ssid, const std::string& password)> cb) {
@@ -211,7 +244,7 @@ void BleProvisioning::send_data(const std::string& json_data) {
         return;
     }
 
-    int rc = ble_gatts_notify_custom(conn_handle_, characteristic_val_handle_, om);
+    int rc = ble_gatts_notify_custom(conn_handle_, gatt_svr_chr_val_handle, om);
     if (rc != 0) {
         ESP_LOGE(TAG, "Failed to send notification: %d", rc);
     } else {
